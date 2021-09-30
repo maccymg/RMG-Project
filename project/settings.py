77a7ef90fd275ccec9eb77fe7195238c4b92991c
maxmghope <maccymg123@gmail.com>
2021-09-30 15:12:14 +0100

    a3

--- project/settings.py
+++ project/settings.py
@@ -14,6 +14,7 @@ import django_on_heroku
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
